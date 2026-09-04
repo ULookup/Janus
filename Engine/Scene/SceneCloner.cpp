e518cf93302af969bbf5cdf84d2a36a4b8f7b7f6
YHaoYang <CHBuLookup@outlook.com>
2026-09-04 16:07:01 +0800

    feat(scene): clone authoring state through persistence contract

---
+++ Engine/Scene/SceneCloner.cpp
@@ -0,0 +1,32 @@
+#include "Scene/SceneCloner.h"
+
+#include "Scene/SceneDeserializer.h"
+#include "Scene/SceneSerializer.h"
+
+#include <utility>
+
+namespace Janus
+{
+
+Result<std::unique_ptr<Scene>> SceneCloner::Clone(
+    const Scene& source)
+{
+    auto serialized = SceneSerializer::Serialize(source);
+    if (!serialized)
+    {
+        return Result<std::unique_ptr<Scene>>::Failure(
+            serialized.GetError());
+    }
+
+    auto clone = SceneDeserializer::Deserialize(serialized.Value());
+    if (!clone)
+    {
+        return Result<std::unique_ptr<Scene>>::Failure(
+            clone.GetError());
+    }
+
+    return Result<std::unique_ptr<Scene>>::Success(
+        std::move(clone).Value());
+}
+
+} // namespace Janus
